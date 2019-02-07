fb86dfeaf2370386e69cb997e38c1cb6e938b1e9
ToxicXGreen <36926768+ToxicXGreen@users.noreply.github.com>
2019-02-07 07:40:42 -0600

    Add files via upload

--- Russian.java
+++ Russian.java
@@ -4,13 +4,15 @@ public class Russian {
 	private int age;
 	private String name;
 	private String gender;
+	private Color color;
 	private int x1;
 	private int y1;
 	
-	public Russian(int a, String n, String gen, Graphics g, int x1, int y1) {
+	public Russian(int a, String n, String gen, Graphics g, Color c, int x1, int y1) {
 		this.age = a;
 		this.name = n;
 		this.gender = gen;
+		this.color = c;
 		this.x1 = x1;
 		this.y1 = y1;
 	}
@@ -18,32 +20,40 @@ public class Russian {
 	public int getAge() { return age; }
 	public String getName() { return name; }
 	public String getGender() { return gender; }
+	public Color getColor() { return color; }	
 	public int getX1() { return x1; }
 	public int getY1() { return y1; }
-	public String getRank()  { return ""; }
-	public String getBranchOfService() { return ""; }
-	public int getYearsServed() { return 0; }
-	public int getNumOfTroops() { return 0; }
-	
+
 	public void setAge(int age) { this.age = age; }
 	public void setName(String name) { this.name = name; }
 	public void setGender(String gender) { this.gender = gender; }
+	public void setColor(Color color) { this.color = color; }
 	public void setX1(int x1) { this.x1 = x1; }
 	public void setY1(int y1) { this.y1	= y1; }
-	public void setRank(String rank) {}
-	public void setBranchOfService(String branchOfService) {}
-	public void setYearsServed(int yearsServed) {}
-	public void setNumOfTroops(int numOfTroops) {}
 	
 	public void greeting() {
 		System.out.println("Hello Comrade, my name is " + this.name + ". i am a " + this.gender + ". I am " + this.age + " years old.\n");
 	}
 	
 	public void drawRussian(Graphics g) {
-		
 		g.fillOval(this.getX1(), this.getY1(), 20, 20);
 		
+		g.setColor(new Color(234, 221, 157));
+		g.fillOval(this.getX1() +1, this.getY1()+1, 18, 18);
+		
+		g.setColor(color.BLACK);
 		g.fillRect(this.getX1(), this.getY1() + 20, 20, 25);
-		g.fillRect(this.getX1(), this.getY1() + 45, 20, 25);
+		
+		g.setColor(color.WHITE);
+		g.fillRect(this.getX1() +1, this.getY1() + 21, 18, 23);
+		
+		g.setColor(color.BLACK);
+		g.fillRect(this.getX1(), this.getY1() + 44, 20, 25);
+		
+		g.setColor(color.green);
+		g.fillRect(this.getX1() +1, this.getY1() + 45, 18, 23);
+		
+		g.setColor(color.black);
+		g.drawLine(this.getX1() +9, this.getY1() +52, this.getX1() +9, this.getY1() +68);
 	}
 }
